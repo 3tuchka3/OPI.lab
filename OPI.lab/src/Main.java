2d2c0a78d81145f4902202f52fa907cb99aff068
Vadim <88191111+3tuchka3@users.noreply.github.com>
2023-01-23 20:01:00 +0300

    Добавлен вывод общей информации о задании

--- OPI.lab/src/Main.java
+++ OPI.lab/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("При выполнении задания своего варианта нужно вписать номер и название своего варианта, а также свои имя и фамилию.");
     }
 }
